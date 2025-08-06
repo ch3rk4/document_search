7829493a5bf9dd03d98e65e34addcd65a2e70432
ch3rk4 <pety.shurupov@gmail.com>
2025-08-06 10:56:00 +0300

    create migrations

--- src/document_search/settings.py
+++ src/document_search/settings.py
@@ -30,8 +30,8 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    'documents',
-   # 'search_engine',
+    'doc_storage',
+    'search_service',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
